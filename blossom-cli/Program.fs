35355b73c28d6e5ee90d788b4a749a2b04198b2a
Simon Pascoe <simon.j.pascoe@gmail.com>
2021-02-05 23:34:53 +0800

    Update program.fs

--- blossom-cli/Program.fs
+++ blossom-cli/Program.fs
@@ -1,42 +1,43 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿open Argu
 open System
-open Argu
 
-type OperatingMode = Interactive | LSP | Http
+type OperatingMode = Interactive | LSP | Http | PP
 
 type Arguments =
   | [<Unique>] Interactive of file:string
-  | [<Unique>] Http of file:string
+  | [<Unique>] Http of file:string * port:int
+  //| [<Unique>] PP of inputType:FileFormat * outputType:FileFormat * input:string * output:string
   | [<Unique>] [<Hidden>] LSP
-  | Port of int
   with
     interface IArgParserTemplate with
       member s.Usage =
         match s with
           | Interactive _ -> "launch interactive console for <file>."
           | Http (_) -> $"start in-process webserver for <file>, on <port> (default: {WebEngine.defaultPort})."
+//        | PP _ -> "run pretty printing engine / transcoder."
           | LSP -> "start persistent executable for language server."
-          | Port _ -> "override <port> for webserver"
 
 [<EntryPoint>]
 let main argv =
     printfn "Blossom 0.1"
-    let parser = ArgumentParser.Create<Arguments>()
+    let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
+    let parser = ArgumentParser.Create<Arguments>(errorHandler = errorHandler)
     let results = parser.Parse argv
 
     // prefer interactive over http
-    let isInteractive = results.TryGetResult Interactive
-    let isHttp = results.TryGetResult Http
+    let isInteractive = results.Contains Interactive
+    let isHttp = results.Contains Http
     let isLSP = results.Contains LSP
+//  let isPP = results.Contains PP
 
-    if isLSP
-      then LspEngine.lsp ()
-      else match isInteractive with
-            | Some fn -> ReplEngine.repl1 fn
-            | None -> match isHttp with
-                        | Some fn -> let port = results.TryGetResult Port
-                                     WebEngine.web fn port
-                        | None -> ReplEngine.repl ReplEngine.State.Default
+    match true with
+      | _ when isLSP  -> LspEngine.lsp()
+      | _ when isHttp -> let fn, port = results.GetResult Http
+                         WebEngine.web fn port
+//    | _ when isPP   -> let it, ot, ifn, ofn = results.GetResult PP
+//                       PrettyPrint.pp it ot ifn ofn
+      | _ when isInteractive -> let fn = results.GetResult Interactive
+                                ReplEngine.repl1 fn
+      | _ -> ReplEngine.repl ReplEngine.State.Default
 
     0
