4730d03134f768fc76ea8db4377d91be356f2f00
Simon Pascoe <simon.j.pascoe@gmail.com>
2021-02-08 01:27:52 +0800

    Rudimentary beancount converter #7.

--- blossom-cli/PrettyPrint.fs
+++ blossom-cli/PrettyPrint.fs
@@ -10,23 +10,43 @@ open Reports
 
 type FileFormat = Blossom | Beancount | Ledger | Knut
 
-let blossom2beancount outputfn journal =
+let blossom2beancount inputfn outputfn  =
+  // preparation
+  let journal = loadJournal inputfn
+
+  // validation
+  match journal.Meta.Convention with
+    | Some Financial5 | Some Financial7 -> true
+    | _ -> failwith "Can only convert Financial5 or Financial7 journals"
+    |> ignore
+
   // header
+  let oc = match journal.Meta.Commodity with | Some (Commodity c) -> c | _ -> "USD"
   let header = [
     $"option \"title\" \"{journal.Meta.Name}\""
-    $"option \"operating_currency\" \"USD\""
+    $"option \"operating_currency\" \"{oc}\""
+    $"option \"render_commas\" \"TRUE\""
     $"option \"name_assets\" \"Asset\""
     $"option \"name_expenses\" \"Expense\""
     $"option \"name_liabilities\" \"Liability\""
     $"option \"name_expenses\" \"Expense\""
+    $"option \"name_income\" \"Income\""
   ]
 
+  // commodities
+  let safeCommodity (com : string) =
+    let com1 = com.ToUpper().Replace(".", "")
+    com1
+
   // accounts
-  let safeAccount (acc : string) = acc.Replace(" ", "-").Replace("/", "-")
+  let safeAccount (acc : string) =
+    let acc1 = acc.Replace(" ", "-").Replace("/", "-")
+    let acc2 = acc1.Replace("Payable:", "Liability:").Replace("Receivable:", "Asset:")
+    acc2
 
   let date0 = journal.Register |> Map.toList |> List.map fst |> List.min
   let (MetaResultSet accountList) = meta id Accounts journal
-  let accounts = [for account in accountList
+  let accounts = [for account in (accountList |> Set.filter (fun x -> not(x.Contains("/"))))
                     do $"""{date0.ToString("yyyy-MM-dd")} open {safeAccount account}"""]
 
   // entries
@@ -37,25 +57,31 @@ let blossom2beancount outputfn journal =
       let s1 = match e.Payee with | Some x -> x | None -> e.Narrative
       let s2 = match e.Payee with | Some x -> e.Narrative | None -> ""
       let r1 = $"{dts} * \"{s1}\" \"{s2}\""
-      let am2s = function V (a, Commodity b) -> $"{a} {b}" | T _ -> "" | X _ -> ""
-      let ac2s = function Account x -> x | VirtualisedAccount (a,b) -> $"{a}-{b}"
+      let am2s = function | V (a, Commodity b) -> $"{a} {safeCommodity b}"
+                          | T ((q1, Commodity c1), (q2, Commodity c2)) -> $"{q1} X{safeCommodity c1} {{{q2} {safeCommodity c2}}}"
+                          | X _ -> ""
+      let ac2s = function Account x -> x | VirtualisedAccount (a, _) -> a
       let rs2 = [for (a,am,_) in e.Postings
-                  do $"  {ac2s a|>safeAccount}    {am2s am}".TrimEnd()]
+                  do $"  {ac2s a |> safeAccount}    {am2s am}".TrimEnd()]
       [r1] @ rs2
     es |> List.collect g
   let entries = rs |> List.collect (fun (d,e) -> f d e)
 
+  // prices
+  let prices = journal.Prices |> Map.toList
+                              |> List.collect (fun ((Commodity q1, Commodity q2), ps) -> ps |> Map.toList |> List.map (fun (dt,p) -> dt,q1,p,q2))
+                              |> List.map (fun (dt,q1,p,q2) -> let dts = dt.ToString("yyyy-MM-dd")
+                                                               $"{dts} price X{safeCommodity q1} {p} {safeCommodity q2}")
 
 
   // build it and write it
-  let content = header @ accounts @ entries
+  let content = header @ accounts @ entries @ prices
   File.WriteAllLines(outputfn, content)
 
 
 let pp it ot ifn ofn =
   match it, ot with
-    | Blossom, Beancount ->
-        loadJournal ifn |> blossom2beancount ofn
+    | Blossom, Beancount -> blossom2beancount ifn ofn
     | _ -> failwith $"Conversion of {it} -> {ot} is unsupported at this time."
 
 
