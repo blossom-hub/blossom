b0ede654d8ecef76e4fa7e79e4ee13ad705cef9f
simonjpascoe <simon.j.pascoe+github@gmail.com>
2022-01-14 00:28:53 +0800

    Add initial webserver draft to support integrations

--- blossom-cli/Program.fs
+++ blossom-cli/Program.fs
@@ -2,17 +2,15 @@
 open System
 open PrettyPrint
 
-type OperatingMode = Interactive | LSP | Http | PP
+type OperatingMode = Interactive | Http | PP
 
 type HttpArgs =
- | [<MainCommand; ExactlyOnce; Last>] File of journal:string
  | Port of port:int
  with
   interface IArgParserTemplate with
     member s.Usage =
       match s with
-        | File _ -> "Journal file for processing"
-        | Port _ -> $"Port for webservice, default: {WebEngine.defaultPort}."
+        | Port _ -> "Port for webservice"
 and PPArgs =
   | [<ExactlyOnce>] InputFile of journal:string
   | [<ExactlyOnce>] OutputFile of string
@@ -26,17 +24,9 @@ and PPArgs =
             | OutputFile _ -> $"Output file for pretty printing."
             | InputType _  -> $"Type of input file."
             | OutputType _ -> $"Type to use for output file."
-and LSPArgs =
-  | Dummy
-    with
-      interface IArgParserTemplate with
-        member s.Usage =
-          match s with
-            | Dummy -> "Test flag"
 and Arguments =
   | [<CliPrefix(CliPrefix.None)>] Http of ParseResults<HttpArgs>
   | [<CliPrefix(CliPrefix.None)>] PP of ParseResults<PPArgs>
-  | [<CliPrefix(CliPrefix.None)>] [<Hidden>] LSP of ParseResults<LSPArgs>
   | [<MainCommand; Last>] File of journal:string
   with
    interface IArgParserTemplate with
@@ -44,35 +34,32 @@ and Arguments =
        match s with
          | Http _ -> "Start the http server."
          | PP _   -> "Transcode and pretty print files."
-         | LSP _   -> "Start the Language Service for VSCode etc."
-         | File _   -> "Provide a file to start up interactive mode."
+         | File _ -> "Provide a file to start up interactive mode."
 
 
 [<EntryPoint>]
 let main argv =
-    printfn "Blossom 0.5"
     let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
     let parser = ArgumentParser.Create<Arguments>(errorHandler = errorHandler)
     let results = parser.Parse argv
 
     let isHttp = results.Contains Http
-    let isLSP = results.Contains LSP
     let isPP = results.Contains PP
 
     let input = results.TryGetResult File
 
     match true with
-      | _ when isLSP  -> LspEngine.lsp()
       | _ when isHttp -> let httpargs = results.GetResult Http
                          let port = httpargs.GetResult Port
-                         WebEngine.web input port
+                         WebEngine.web port
       | _ when isPP   -> let ppargs = results.GetResult PP
                          let ifn = ppargs.GetResult InputFile
                          let it = ppargs.GetResult InputType
                          let ot = ppargs.GetResult OutputType
                          let ofn = ppargs.GetResult OutputFile
                          PrettyPrint.pp it ot ifn ofn
-      | _ -> match input with
+      | _ -> printfn "Blossom 0.5"
+             match input with
                | Some fn -> ReplEngine.repl1 fn
                | None    -> ReplEngine.repl ReplEngine.State.Default
     0
--- blossom-cli/WebEngine.fs
+++ blossom-cli/WebEngine.fs
@@ -1,6 +1,85 @@
 module WebEngine
 
-let defaultPort = 8086
+open System
+open System.Collections.Generic
 
-let web filename port =
-  failwith "Not yet implemented"
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.Utils.Collections
+
+open ReplParser
+open ParserShared
+open Types
+open Journal
+open Reports
+open Renderers
+
+let STATELOCK = Object()
+
+type State = {
+  Journals: Dictionary<int, string * Journal>
+}
+  with
+    static member Default = {
+      Journals = new Dictionary<int, string * Journal>()
+    }
+    member this.nextId () = 
+      lock STATELOCK (fun () -> let keys = this.Journals.Keys
+                                if Seq.isEmpty keys then 0 else Seq.max keys |> fun k -> k + 1)
+    member this.getFilename index = this.Journals.TryGetValue index |> function | true, (fn, _) -> Some fn | _ -> None
+    member this.getIndex filename = this.Journals |> Seq.tryPick (fun (KeyValue(k, (fn, _))) -> if fn = filename then Some k else None)
+    member this.getJournal index = this.Journals |> Seq.tryPick (fun (KeyValue(i, (_, j))) -> if i = index then Some j else None)
+    member this.getJournal filename = this.Journals |> Seq.tryPick (fun (KeyValue(_, (fn, j))) -> if fn = filename then Some j else None)
+
+let load (state: State) filename =
+  lock STATELOCK (fun () -> 
+    let usedId = match state.getIndex filename with 
+                  | None   -> state.nextId()
+                  | Some i -> i
+    let journal = loadJournal false DateTime.Now filename
+    state.Journals.[usedId] <- (filename, journal)
+    usedId
+ )
+
+let execute (state: State) (index: int, input) =
+  let result = runParser parse () (FromString input)
+
+  let withJournal op = 
+    let journal = state.getJournal index
+    match journal with 
+      | Some j -> op j |> Choice1Of2
+      | None   -> Choice2Of2 "Invalid journal id"
+
+  match result with 
+    | Balances (filter, request)         -> withJournal <| balances Tabular.renderText filter request
+    | Journal (filter, request)          -> withJournal <| journal Tabular.renderText filter request
+    | BalanceSeries (filter, request)    -> withJournal <| balanceSeries Tabular.renderText filter request
+    | LotAnalysis (filter, request)      -> withJournal <| lotAnalysis Tabular.renderText filter request
+    | HoldingsAnalysis filter            -> withJournal <| holdingsAnalysis Tabular.renderText filter true
+    | Check request                      -> withJournal <| checkJournal Tabular.renderText request
+    | Meta request                       -> withJournal <| meta HumanReadable.renderMetaResult request
+    | _ -> Choice2Of2 "Unsupported via http!"
+
+let web port =
+  let config = { defaultConfig with bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" port] }
+
+  let state = State.Default
+
+  startWebServer config <| 
+    choose [
+      GET >=> choose [
+        path "/rest" >=> OK "In peace" 
+        pathScan "/rest/load/%s" (load state >> string >> OK)
+        pathScan "/rest/%d/filename" (fun i -> state.getFilename i |> Option.defaultValue "" |> OK)
+      ]
+      POST >=> choose [
+        pathScan "/rest/%d/execute" (fun i -> request (fun r -> let command = fst r.form.[0]
+                                                                let res = execute state (i, command) 
+                                                                match res with 
+                                                                  | Choice1Of2 a -> OK (String.concat Environment.NewLine a)
+                                                                  | Choice2Of2 b -> OK b))
+      ]      
+      OK "Hi there"
+    ]
